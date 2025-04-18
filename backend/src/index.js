25db0a61570bd2bbdd73563194a31e24746ffd5e
Tushar <tusharraghav12345@gmail.com>
2025-04-18 11:33:22 +0530

    more changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,6 +18,9 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+
 app.use(
   cors({
     origin: "http://localhost:5173",
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -65,7 +65,7 @@ const SignUpPage = () => {
                 <input
                   type="text"
                   className={`input input-bordered w-full pl-10`}
-                  placeholder="John Doe"
+                  placeholder="Tushar Raghav"
                   value={formData.fullName}
                   onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
                 />
