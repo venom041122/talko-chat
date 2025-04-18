ae6fa0c89e162f0a0483150d0de241a441284974
Tushar <tusharraghav12345@gmail.com>
2025-04-18 11:47:57 +0530

    more changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+axiosInstance.post(url, data, {
+  maxContentLength: 50 * 1024 * 1024, // 50 MB
+  maxBodyLength: 50 * 1024 * 1024,
+});
+
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
